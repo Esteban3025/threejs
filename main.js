765ff469cbc0e79e168d9fd9c6dee2efdadcd1fd
Nosekp <glo3025pando@gmail.com>
2025-03-26 12:47:52 -0300

    proyectos

---
+++ main.js
@@ -0,0 +1,72 @@
+import * as THREE from 'three';
+
+	import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+	import { KMZLoader } from 'three/addons/loaders/KMZLoader.js';
+
+			let camera, scene, renderer;
+
+			init();
+
+			function init() {
+
+				scene = new THREE.Scene();
+				scene.background = new THREE.Color( 0x999999 );
+
+				const light = new THREE.DirectionalLight( 0xffffff, 3 );
+				light.position.set( 0.5, 1.0, 0.5 );
+
+				scene.add( light );
+
+				camera = new THREE.PerspectiveCamera( 35, window.innerWidth / window.innerHeight, 1, 500 );
+
+				camera.position.y = 5;
+				camera.position.z = 10;
+
+				scene.add( camera );
+
+				const grid = new THREE.GridHelper( 6, 6, 0xffffff, 0x7b7b7b );
+				scene.add( grid );
+
+				const geometry = new THREE.BoxGeometry( 1, 1, 1 ); 
+				const material = new THREE.MeshBasicMaterial( {color: 0x00ff00} ); 
+				const cube = new THREE.Mesh( geometry, material ); 
+				scene.add( cube );
+
+				renderer = new THREE.WebGLRenderer( { antialias: true } );
+				renderer.setPixelRatio( window.devicePixelRatio );
+				renderer.setSize( window.innerWidth, window.innerHeight );
+				document.body.appendChild( renderer.domElement );
+
+				const loader = new KMZLoader();
+				loader.load( './models/kmz/Box.kmz', function ( kmz ) {
+
+					kmz.scene.position.y = 0.5;
+					scene.add( kmz.scene );
+					render();
+
+				} );
+
+				const controls = new OrbitControls( camera, renderer.domElement );
+				controls.addEventListener( 'change', render );
+				controls.update();
+
+				window.addEventListener( 'resize', onWindowResize );
+
+			}
+
+			function onWindowResize() {
+
+				camera.aspect = window.innerWidth / window.innerHeight;
+				camera.updateProjectionMatrix();
+
+				renderer.setSize( window.innerWidth, window.innerHeight );
+
+				render();
+
+			}
+
+			function render() {
+
+				renderer.render( scene, camera );
+
+			}
