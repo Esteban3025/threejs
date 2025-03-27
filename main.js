edf4651e417d649ae1757cc771817cf3defa1562
Nosekp <glo3025pando@gmail.com>
2025-03-27 17:07:12 -0300

    plane with a cube and sphere

--- main.js
+++ main.js
@@ -1,70 +1,174 @@
 import * as THREE from 'three';
-const loadingElem = document.querySelector('#loading');
-const progressBarElem = loadingElem.querySelector('.progressbar');
+import {OrbitControls} from 'three/addons/controls/OrbitControls.js';
+import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
 
-const renderer = new THREE.WebGLRenderer();
+const canvas = document.querySelector('#c');
 
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+const renderer = new THREE.WebGLRenderer( { antialias: true, canvas } );
 
 // Camara
-const fov = 75;
+const fov = 45;
 const aspect = 2;  // the canvas default
 const near = 0.1;
-const far = 5;
+const far = 100;
 const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-camera.position.z = 2;
+camera.position.set(0, 10, 20);
 
+// Escena
 const scene = new THREE.Scene();
-// Geometria
-const boxWidth = 1;
-const boxHeight = 1;
-const boxDepth = 1;
-const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
+scene.background = new THREE.Color( 'black' );
+// controles
+const controls = new OrbitControls(camera, canvas);
+controls.target.set(0, 5, 0);
+controls.update();
 
-// Luz
-const color = 0xFFFFFF;
-const intensity = 3;
-const light = new THREE.DirectionalLight(color, intensity);
-light.position.set(-1, 2, 4);
-scene.add(light);
 
-const loadManager = new THREE.LoadingManager();
-const loader = new THREE.TextureLoader(loadManager);
-const texture = loader.load('https://threejs.org/manual/examples/resources/images/flower-1.jpg');
-const xOffset = .1;   // offset by half the texture
-const yOffset = .25;  // offset by 1/4 the texture
-texture.offset.set(xOffset, yOffset);
+const planeSize = 40;
+
+// Texture loader
+const loader = new THREE.TextureLoader();
+const texture = loader.load('./img/checker.png');
+texture.wrapS = THREE.RepeatWrapping;
+texture.wrapT = THREE.RepeatWrapping;
+texture.magFilter = THREE.NearestFilter;
+texture.colorSpace = THREE.SRGBColorSpace;
+const repeats = planeSize / 2;
+texture.repeat.set(repeats, repeats);
+
+// Plane terrain
+const planeGeo = new THREE.PlaneGeometry(planeSize, planeSize);
+const planeMat = new THREE.MeshPhongMaterial({ map: texture, side: THREE.DoubleSide });
+const mesh = new THREE.Mesh(planeGeo, planeMat);
+mesh.rotation.x = Math.PI * -.5;
+scene.add(mesh);
+
+// Cube
+{
+const cubeSize = 4;
+const cubeGeo = new THREE.BoxGeometry(cubeSize, cubeSize, cubeSize);
+const cubeMat = new THREE.MeshPhongMaterial({ color: '#8AC' });
+const mesh = new THREE.Mesh(cubeGeo, cubeMat);
+mesh.position.set(cubeSize + 1, cubeSize / 2, 0);
+scene.add(mesh);
+}
+
+{
+  const sphereRadius = 3;
+  const sphereWidthDivisions = 32;
+  const sphereHeightDivisions = 16;
+  const sphereGeo = new THREE.SphereGeometry(sphereRadius, sphereWidthDivisions, sphereHeightDivisions);
+  const sphereMat = new THREE.MeshPhongMaterial({color: '#CA8'});
+  const mesh = new THREE.Mesh(sphereGeo, sphereMat);
+  mesh.position.set(-sphereRadius - 1, sphereRadius + 2, 0);
+  scene.add(mesh);
+}
+
+
+// setup the helper of the lights
+class ColorGUIHelper {
+
+	constructor( object, prop ) {
+
+		this.object = object;
+		this.prop = prop;
+
+	}
+	get value() {
+
+		return `#${this.object[ this.prop ].getHexString()}`;
+
+	}
+	set value( hexString ) {
+
+		this.object[ this.prop ].set( hexString );
+
+	}
+
+}
+
+class DegRadHelper {
+
+	constructor( obj, prop ) {
+
+		this.obj = obj;
+		this.prop = prop;
+
+	}
+	get value() {
+
+		return THREE.MathUtils.radToDeg( this.obj[ this.prop ] );
+
+	}
+	set value( v ) {
+
+		this.obj[ this.prop ] = THREE.MathUtils.degToRad( v );
+
+	}
+
+}
+
+	// setup the lights
+
+function makeXYZGUI(gui, vector3, name, onChangeFn) {
+	const folder = gui.addFolder(name);
+	folder.add(vector3, 'x', -10, 10).onChange(onChangeFn);
+	folder.add(vector3, 'y', 0, 10).onChange(onChangeFn);
+	folder.add(vector3, 'z', -10, 10).onChange(onChangeFn);
+	folder.open();
+}
+
+{
+	const color = 0xFFFFFF;
+	const intensity = 150;
+	const light = new THREE.SpotLight( color, intensity );
+	light.position.set(0, 10, 0);
+	scene.add( light );
+	scene.add( light.target );
+
+	const helper = new THREE.SpotLightHelper(light);
+	scene.add(helper);
+
+	function updateLight() {
+		helper.update();
+	}
+
+	const gui = new GUI();
+	gui.addColor( new ColorGUIHelper( light, 'color' ), 'value' ).name( 'color' );
+	gui.add(new DegRadHelper(light, 'angle'), 'value', 0, 90).name('angle').onChange(updateLight);
+	gui.add( light, 'intensity', 0, 150, 0.01 );
+	gui.add( light, 'distance', 0, 40 ).onChange(updateLight);
+	gui.add(light, 'penumbra', 0, 1, 0.01);
+	
+	makeXYZGUI( gui, light.position, 'position', updateLight );
+}
+
+function resizeRendererToDisplaySize( renderer ) {
+
+	const canvas = renderer.domElement;
+	const width = canvas.clientWidth;
+	const height = canvas.clientHeight;
+	const needResize = canvas.width !== width || canvas.height !== height;
+	if ( needResize ) {
+
+		renderer.setSize( width, height, false );
+
+	}
+
+	return needResize;
+
+}
 
 renderer.render(scene, camera);
 
-	const material = new THREE.MeshPhongMaterial({
-	map: texture,
-  	});
+function render() {
 
-	loadManager.onLoad = () => {
-		loadingElem.style.display = 'none';
-		const cube = new THREE.Mesh(geometry, material);
-		scene.add(cube);
-		cubes.push(cube);  // add to our list of cubes to rotate
-	};
+	if ( resizeRendererToDisplaySize( renderer ) ) {
 
-	loadManager.onProgress = (urlOfLastItemLoaded, itemsLoaded, itemsTotal) => {
-		const progress = itemsLoaded / itemsTotal;
-		progressBarElem.style.transform = `scaleX(${progress})`;
-	  };
+		const canvas = renderer.domElement;
+		camera.aspect = canvas.clientWidth / canvas.clientHeight;
+		camera.updateProjectionMatrix();
 
-	const cubes = [];
-
-function render(time) {
-	time *= 0.001;
-
-	cubes.forEach((cube, ndx) => {
-		const speed = 1 + ndx * .1;
-		const rot = time * speed;
-		cube.rotation.x = rot;
-		cube.rotation.y = rot;
-	});
+	}
 
 	renderer.render(scene, camera);
 
